f719a2b9794e5997300dc343220a82eaf6d16ab3
TalonStudios <113108821+TalonStudios@users.noreply.github.com>
2024-11-12 10:51:32 -0500

    Add files via upload

---
+++ startbootstrap-sb-admin-gh-pages/startbootstrap-sb-admin-gh-pages/assets/demo/chart-bar-demo.js
@@ -0,0 +1,53 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+var myAmount1 = 2
+var myAmount2 = 4
+
+var myAmountSleep = 6
+var myAmount3 = 8
+var myAmount4 = 10
+var myAmount5 = 12
+// Bar Chart Example
+var ctx = document.getElementById("myBarChart");
+var myLineChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["January", "February", "SLEEPY", "April", "May", "June"],
+    datasets: [{
+      label: "Revenue",
+      backgroundColor: "rgba(2,117,216,1)",
+      borderColor: "rgba(2,117,216,1)",
+      data: [myAmount1, myAmount2, myAmountSleep, myAmount3, myAmount4, myAmount5],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'month'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 6
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 20,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          display: true
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
